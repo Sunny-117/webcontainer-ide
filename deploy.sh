2fb12d50343c319f480437c13c642be2ea00102f
Sunny-117 <zhiqiangfu6@gmail.com>
2025-12-03 21:32:42 +0800

    feat: webcontainer ide

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ cd dist
 git init
 git add .
 git commit -m 'deploy'
-git push -f https://github.com/Sunny-117/web-coding.git main:gh-pages
+git push -f https://github.com/Sunny-117/web-container-ide.git main:gh-pages
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/web-coding/',
+  base: '/web-container-ide/',
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
